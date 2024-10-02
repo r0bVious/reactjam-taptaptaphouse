1e3b15b01c8428ed8bcb16c5a88ad7fa2c55fba2
r0bVious <robert.spano@gmail.com>
2024-10-02 21:57:39 +0900

    feat: three taps per throw

--- src/components/Tap.tsx
+++ src/components/Tap.tsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { useState } from "react";
 
 type TapProps = { throwDrink: () => void };
 
 const Tap: React.FC<TapProps> = ({ throwDrink }) => {
+  const [fillDrink, setFillDrink] = useState<number>(0);
+
+  const pourDrink = () => {
+    console.log(fillDrink);
+    if (fillDrink > 2) {
+      throwDrink();
+      setFillDrink(0);
+    } else setFillDrink((prev) => prev + 1);
+  };
+
   return (
-    <div className="tap" onClick={throwDrink}>
+    <div className="tap" onClick={pourDrink}>
       Tap
     </div>
   );
